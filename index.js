3c02b46bef61d1b5b768157068aadc12eee8b8ff
mr-jingo <85439298+mr-jingo@users.noreply.github.com>
2021-11-05 16:21:04 +0100

    passport.authenticate back for /movies

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ app.get('/', (req, res) => {
   res.send('Welcome to myFlix!');
 });
 
-app.get('/movies', (req, res) => {
+app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
   Movies.find()
     .then((movies) => {
       res.status(201).json(movies);
